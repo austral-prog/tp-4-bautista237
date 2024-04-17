ee49d9d864e4973015f75b3597f397f407e23fbd
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-04-17 19:19:51 -0300

    Update line.py

--- line.py
+++ line.py
@@ -6,7 +6,7 @@ def line():
     X2=float(input("Ingrese el coeficiente X2: "))
     Y1=(AX1+B)
     Y2=(AX2+B)
-    distancia=(sqrt((X2-X1)2+(Y2-Y1)2))
+    distancia=(sqrt((X2-X1)**2+(Y2-Y1)**2))
     print("El coeficiente A de su ecuación de la recta es:",A)
     print("El coeficiente B de su ecuación de la recta es:",B)
     print("El coeficiente X1 de su ecuación de la recta es:",X1)
